8bd33426b041277b19a35b2acfd1d02cbe87bdc3
shlyoki <114242981+shlyoki@users.noreply.github.com>
2025-10-26 00:02:25 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'node:path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',                // critical for Vercel
+  base: "/", // important for Vercel root deployments
   resolve: {
-    alias: { '@': path.resolve(__dirname, 'src') }
-  }
-})
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
